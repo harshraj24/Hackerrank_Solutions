d0538d4e33838dcf830a90e26d614142f18cf4ba
Harsh Raj <me@harshraj.xyz>
2020-09-18 22:56:36 +0530

    # Hackerrank 30 days challenge day 10 Solution

---
+++ 30_Days_Day_10.py
@@ -0,0 +1,22 @@
+# Given a base- integer, , convert it to binary (base-). Then find and print the base- integer denoting the maximum number of consecutive 's in 's binary representation.
+
+# Solution
+
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+
+
+if __name__ == '__main__':
+    n = int(input())
+    count = 0
+    while n:
+        n &= n << 1
+        count += 1
+
+    print(count)

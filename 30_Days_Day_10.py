dd997d73d7ac5e6114fa37f3dbc64746116c653a
Harsh Raj <54536823+harshraj24@users.noreply.github.com>
2020-09-22 22:33:38 +0530

    Update 30_Days_Day_10.py

--- 30_Days_Day_10.py
+++ 30_Days_Day_10.py
@@ -1,6 +1,6 @@
 # Given a base- integer, , convert it to binary (base-). Then find and print the base- integer denoting the maximum number of consecutive 's in 's binary representation.
 
-# Solution
+
 
 #!/bin/python3
 

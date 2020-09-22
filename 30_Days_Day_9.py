44e0cf98e9a9f630d0d8a3c7d05d8148f9b82040
Harsh Raj <54536823+harshraj24@users.noreply.github.com>
2020-09-22 22:32:28 +0530

    Update 30_Days_Day_9.py

--- 30_Days_Day_9.py
+++ 30_Days_Day_9.py
@@ -12,9 +12,9 @@ import random
 import re
 import sys
 
-# Complete the factorial function below.
+
 def factorial(n):
-    if n==1:
+    if (n==1):
         return n
     else:
         return(n*factorial(n-1))
